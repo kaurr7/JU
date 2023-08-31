8403b62fa2e093a04beea83ca04accf26ea5b2a9
kaurr7 <129803777+kaurr7@users.noreply.github.com>
2023-08-31 19:15:20 +0530

    Add files via upload

---
+++ Students.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Students
+{
+    public static void main(String args[])
+    {
+        //F1: user input assignment name
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the assignment name: ");
+        String assignmentName = scanner.nextLine();
+
+    }
+}
