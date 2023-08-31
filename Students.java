935565ecd2c5fbad2bc346638c4c019d011c0a56
kaurr7 <129803777+kaurr7@users.noreply.github.com>
2023-08-31 19:25:47 +0530

    Update Students.java
    task f2 updated

--- Students.java
+++ Students.java
@@ -8,5 +8,16 @@ class Students
         System.out.println("Please enter the assignment name: ");
         String assignmentName = scanner.nextLine();
 
+        //F2 : input students marks for assignment 
+        int[] marksList = new int[30];
+        for (int i=0; i<30; i++)
+        { 
+            System.out.println("Enter mark for student " + (i + 1) + ": ");
+                int mark = scanner.nextInt();
+                if (mark >=0 && mark <=30)
+                {
+                    marksList[i] = mark;       
+                }
+            }
     }
 }
